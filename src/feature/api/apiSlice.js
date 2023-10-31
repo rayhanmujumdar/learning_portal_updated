6546e62d0ad08d9c5b4be5847fba2d8a569338c8
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2023-11-01 02:05:12 +0600

    backend host url added

--- src/feature/api/apiSlice.js
+++ src/feature/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
 import { loggedOut } from "../auth/authSlice";
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:3000/api/v1",
+  baseUrl: "https://learning-portal-application.onrender.com/api/v1",
   prepareHeaders: async (headers, { getState }) => {
     const token = await getState().auth.accessToken;
     if (token) {
